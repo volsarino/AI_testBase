a99a6547fbafa236ea2c2cbc297eae4d22636bd2
Yu1tata <chyi24020@g.nihon-u.ac.jp>
2026-06-11 14:14:47 +0900

    AImodel_prot

---
+++ ResNet.py
@@ -0,0 +1,63 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+class Residual(nn.Module):
+    def __init__(self,num_channels,use_1x1conv=False,strides=1):
+        super().__init__()
+        self.conv1=nn.LazyConv2d(num_channels,kernel_size=3,padding=1,stride=strides)
+        self.conv2=nn.LazyConv2d(num_channels,kernel_size=3,padding=1)
+        if use_1x1conv:
+            self.conv3=nn.LazyConv2d(num_channels,kernel_size=1,stride=strides)
+        else:
+            self.conv3=None
+        self.bn1=nn.LazyBatchNorm2d()
+        self.bn2=nn.LazyBatchNorm2d()
+
+    def forward(self,X):
+        Y=F.relu(self.bn1(self.conv1(X)))
+        Y=self.bn2(self.conv2(Y))
+        if self.conv3:
+            X=self.conv3(X)
+        Y+=X
+        return F.relu(Y)
+
+class ResNet(nn.Module):
+    def b1(self):
+        return nn.Sequential(
+            nn.LazyConv2d(64,kernel_size=7,stride=2,padding=3),
+            nn.LazyBatchNorm2d(),nn.ReLU(),nn.MaxPool2d(kernel_size=3,stride=2,padding=1)
+        )
+    
+    def block(self,num_residuals,num_channels,first_block=False):
+        blk=[]
+        for i in range(num_residuals):
+            if i==0 and not first_block:
+                blk.append(Residual(num_channels,use_1x1conv=True,strides=2))
+            else:
+                blk.append(Residual(num_channels))
+        return nn.Sequential(*blk)
+    
+    def _init_weights(self,m):
+        if type(m) in [nn.Conv2d,nn.Linear]:
+            nn.init.xavier_uniform_(m.weight)
+
+    def __init__(self,arch,Ir=0.1,num_classes=10):
+        super(ResNet,self).__init__()
+        #self.save_hyperparameters()
+        self.net=nn.Sequential(self.b1())
+        for i,b in enumerate(arch):
+             self.net.add_module(f'b{i+2}', self.block(*b, first_block=(i==0)))
+        
+        self.last_layer=nn.Sequential(
+            nn.AdaptiveAvgPool2d((1,1)),nn.Flatten(),nn.LazyLinear(num_classes)
+        )
+        self.net.apply(self._init_weights)
+
+    def forward(self,X):
+
+        for name,layer in self.net.named_children():
+            X=layer(X)
+
+        return X
+
